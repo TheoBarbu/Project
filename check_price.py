c9e43e4a300827fa416294295a4650e68790b7cf
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:45:07 +0300

    check_price.py

--- check_price.py
+++ check_price.py
@@ -1,26 +1,58 @@
 import unittest
+from app.check_price import GroceryCart
+import softest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class GroceryCart:
-    def __init__(self):
-        self.items = {}
+class TestShopifyGroceriesPage(unittest.TestCase):
+    def setUp(self) -> None:
+        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.chrome.maximize_window()
+        self.chrome.get("https://www.freshful.ro/")
+        self.chrome.implicitly_wait(2)
 
-    def add_item(self, item, quantity):
-        if item in self.items:
-            self.items[item] += quantity
-        else:
-            self.items[item] = quantity
+    def tearDown(self) -> None:
+        self.chrome.quit()
 
-    def get_all_items(self):
-        return list(self.items.keys())
 
-    def count_items(self):
-        return len(self.items)
+class TestGroceryCart(unittest.TestCase):
+    def setUp(self):
+        self.cart = GroceryCart()
+        self.item1 = Item("apples", 0.5)
+        self.item2 = Item("bananas", 0.3)
 
-    def total_cost(self):
-        return sum([item.price * quantity for item, quantity in self.items.items()])
+    def test_add_item(self):
+        self.cart.add_item(self.item1, 3)
+        self.assertEqual(self.cart.count_items(), 1)
 
-    def get_item_price(self, item):
-        return item.price
+        self.cart.add_item(self.item2, 2)
+        self.assertEqual(self.cart.count_items(), 2)
+
+    def test_total_cost(self):
+        self.cart.add_item(self.item1, 3)
+        self.cart.add_item(self.item2, 2)
+
+    def test_get_all_items(self):
+        self.cart.add_item(self.item1, 3)
+        self.cart.add_item(self.item2, 2)
+        self.assertCountEqual(self.cart.get_all_items(), [self.item1, self.item2])
+
+    def test_get_item_price(self):
+        self.assertEqual(self.cart.get_item_price(self.item1), 0.5)
+        self.assertEqual(self.cart.get_item_price(self.item2), 0.3)
+
+
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
