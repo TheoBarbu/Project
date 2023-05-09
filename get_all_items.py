4a91dfded2f38a8fd17dc165c2264dd88711fd4f
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:48:27 +0300

    get_all_items.py

--- get_all_items.py
+++ get_all_items.py
@@ -1,22 +1,52 @@
 import unittest
 
-class GroceryCart:
-    def __init__(self):
-        self.items = {}
+from app.get_all_items import GroceryCart
+import softest
 
-    def add_item(self, item, quantity):
-        if item in self.items:
-            self.items[item] += quantity
-        else:
-            self.items[item] = quantity
-
-    def get_all_items(self):
-        return list(self.items.keys())
-
-    def count_items(self):
-        return len(self.items)
-
-    def total_cost(self):
-        return sum([item.price * quantity for item, quantity in self.items.items()])
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
+class TestShopifyGroceriesPage(unittest.TestCase):
+    def setUp(self) -> None:
+        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.chrome.maximize_window()
+        self.chrome.get("https://www.freshful.ro/")
+        self.chrome.implicitly_wait(2)
+
+    def tearDown(self) -> None:
+        self.chrome.quit()
+
+
+class TestGroceryCart(unittest.TestCase):
+    def setUp(self):
+        self.cart = GroceryCart()
+        self.item1 = Item("apples", 0.5)
+        self.item2 = Item("bananas", 0.3)
+
+    def test_add_item(self):
+        self.cart.add_item(self.item1, 3)
+        self.assertEqual(self.cart.count_items(), 1)
+
+        self.cart.add_item(self.item2, 2)
+        self.assertEqual(self.cart.count_items(), 2)
+
+    def test_total_cost(self):
+        self.cart.add_item(self.item1, 3)
+        self.cart.add_item(self.item2, 2)
+
+    def test_get_all_items(self):
+        self.cart.add_item(self.item1, 3)
+        self.cart.add_item(self.item2, 2)
+        self.assertCountEqual(self.cart.get_all_items(), [self.item1, self.item2])
+
+
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+if __name__ == '__main__':
+    unittest.main()
