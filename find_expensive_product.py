24b9b8b477ed0619e35027c9bea161d972738532
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:50:55 +0300

    find_expensive_product.py

--- find_expensive_product.py
+++ find_expensive_product.py
@@ -1,41 +1,7 @@
 import unittest
 
-from app.find_expensive_product import find_expensive_product
-
-import softest
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-class TestShopifyGroceriesPage(unittest.TestCase):
-    def setUp(self) -> None:
-        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        self.chrome.maximize_window()
-        self.chrome.get("https://www.freshful.ro/")
-        self.chrome.implicitly_wait(2)
-
-    def tearDown(self) -> None:
-        self.chrome.quit()
-
-class TestCart(unittest.TestCase):
-
-    def test_find_expensive_product(self):
-        cart = [
-            {'name': 'product1', 'price': 10},
-            {'name': 'product2', 'price': 20},
-            {'name': 'product3', 'price': 30}
-        ]
-
-        result = find_expensive_product(cart)
-        expected_result = {'name': 'product3', 'price': 30}
-        self.assertEqual(result, expected_result)
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-
+def find_expensive_product(cart):
+    most_expensive_product = max(cart, key=lambda x: x['price'])
+    return most_expensive_product
 
 
