f6585b33a52a2b47b198521186531c3c45f2a733
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:46:26 +0300

    find_cheapest_product.py

--- find_cheapest_product.py
+++ find_cheapest_product.py
@@ -1,6 +1,35 @@
 import unittest
 
-def find_cheapest_product(cart):
-    cheapest_product = min(cart, key=lambda x: x['price'])
-    return cheapest_product
+from app.find_cheapest_product import find_cheapest_product
 
+import softest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestShopifyGroceriesPage(unittest.TestCase):
+    def setUp(self) -> None:
+        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.chrome.maximize_window()
+        self.chrome.get("https://www.freshful.ro/")
+        self.chrome.implicitly_wait(2)
+
+    def tearDown(self) -> None:
+        self.chrome.quit()
+
+class TestCart(unittest.TestCase):
+
+    def test_find_cheapest_product(self):
+        cart = [
+            {'name': 'product1', 'price': 10},
+            {'name': 'product2', 'price': 20},
+            {'name': 'product3', 'price': 5}
+        ]
+        result = find_cheapest_product(cart)
+        expected_result = {'name': 'product3', 'price': 5}
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
