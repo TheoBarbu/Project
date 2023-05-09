1af5f1afd5b6dc880cb83203d5b05bba002c86c1
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:51:15 +0300

    get_a_single_order.py

--- get_a_single_order.py
+++ get_a_single_order.py
@@ -1,33 +1,18 @@
 import unittest
 
-from app.get_a_single_order import GroceryItem, GroceryOrder
-import softest
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+class GroceryOrder:
+    def __init__(self, items):
+        self.items = items
 
-
-class TestShopifyGroceriesPage(unittest.TestCase):
-    def setUp(self) -> None:
-        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        self.chrome.maximize_window()
-        self.chrome.get("https://www.freshful.ro/")
-        self.chrome.implicitly_wait(2)
-
-    def tearDown(self) -> None:
-        self.chrome.quit()
+    def total_items(self):
+        return sum([item.quantity for item in self.items])
 
 
-class TestGroceryOrder(unittest.TestCase):
-    def setUp(self):
-        self.item1 = GroceryItem("apples", 0.5, 3)
-        self.item2 = GroceryItem("bananas", 0.3, 2)
-        self.order = GroceryOrder([self.item1, self.item2])
-
-    def test_total_items(self):
-        self.assertEqual(self.order.total_items(), 5)
+class GroceryItem:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
 
-if __name__ == '__main__':
-    unittest.main()
