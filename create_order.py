0ba0e3fe669626141217eca2112bba0132dd1a3e
TheoBarbu <119164076+TheoBarbu@users.noreply.github.com>
2023-05-09 23:50:10 +0300

    create_order.py

--- create_order.py
+++ create_order.py
@@ -1,56 +1,24 @@
-import unittest
+class GroceryOrder:
+    def __init__(self):
+        self.items = {}
 
-from app.create_order import GroceryOrder
-import softest
+    def add_item(self, item, quantity):
+        if item in self.items:
+            self.items[item] += quantity
+        else:
+            self.items[item] = quantity
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+    def remove_item(self, item, quantity):
+        if item not in self.items:
+            raise ValueError("Item not in order")
+        elif quantity > self.items[item]:
+            raise ValueError("Quantity greater than order")
+        else:
+            self.items[item] -= quantity
+            if self.items[item] == 0:
+                del self.items[item]
 
-
-class TestShopifyGroceriesPage(unittest.TestCase):
-    def setUp(self) -> None:
-        self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        self.chrome.maximize_window()
-        self.chrome.get("https://www.freshful.ro/")
-        self.chrome.implicitly_wait(2)
-
-    def tearDown(self) -> None:
-        self.chrome.quit()
-
-
-class TestGroceryOrder(unittest.TestCase):
-    def setUp(self):
-        self.order = GroceryOrder()
-        self.item1 = Item("apples", 0.5)
-        self.item2 = Item("bananas", 0.3)
-
-    def test_add_item(self):
-        self.order.add_item(self.item1, 3)
-        self.assertEqual(self.order.total_items(), 3)
-
-        self.order.add_item(self.item2, 2)
-        self.assertEqual(self.order.total_items(), 5)
-
-    def test_remove_item(self):
-        self.order.add_item(self.item1, 3)
-        self.order.add_item(self.item2, 2)
-
-        self.order.remove_item(self.item1, 2)
-        self.assertEqual(self.order.total_items(), 3)
-
-        self.order.remove_item(self.item1, 1)
-        self.assertEqual(self.order.total_items(), 2)
-
-        with self.assertRaises(ValueError):
-            self.order.remove_item(self.item2, 3)
+    def total_items(self):
+        return sum(self.items.values())
 
 
-class Item:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-
-if __name__ == '__main__':
-    unittest.main()
